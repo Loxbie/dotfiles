853a79eac32ee13688e4e2565ab54138ab44efb4
Loxbie <ole@freirad.at>
2024-01-22 10:56:05 +0100

    Feat: add install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dir=~/dotfiles			# dotfiles directory
+backupdir=~/dotfiles.backup	# dorfiles backup directory
+# list of files/folders to symlink in homedir
+files=".bashrc .config/kitty .config/sway .config/waybar"
+
+
+echo "Creating $bakupdir to backup any existing dotfiles in ~"
+mkdir -p $backupdir
+echo "... done"
+
+cd $dir
+
+# move existing dotfiles in homedir to backupdir, 
+# then create symlinks
+
+for file in $files; do
+	mv ~/$file ${backupdir}/ && echo "Moved $file to ${backupdir}/${file}"
+	ln -s ${dir}/${file} ~/${file} && echo "Created symlink to ~/${file}"
+done
