e4c93e28c24c31a093c523daf86e6edf569a2865
Ole Binder <loxbie@hotmail.com>
2024-03-05 12:37:01 +0100

    Feat: add support for Konsole as the terminal

---
+++ .local/share/konsole/Lox.profile
@@ -0,0 +1,12 @@
+[Appearance]
+ColorScheme=Breeze
+Font=Hack,11,-1,5,50,0,0,0,0,0
+TabColor=27,30,32,0
+
+[General]
+Name=Lox
+Parent=FALLBACK/
+TerminalMargin=1
+
+[Scrolling]
+ScrollBarPosition=2
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 dir=~/dotfiles			# dotfiles directory
 backupdir=~/dotfiles.backup	# dorfiles backup directory
 # list of files/folders to symlink in homedir
-files=".bashrc .config/kitty .config/sway .config/waybar .config/swaynag"
+files=".bashrc .config/kitty .config/sway .config/waybar .config/swaynag .local/share/konsole/Lox.profile .local/share/konsole/Breeze.colorscheme .local/share/konsole/konsolestaterc .config/konsolerc"
 
 
 echo "Creating $bakupdir to backup any existing dotfiles in ~"
